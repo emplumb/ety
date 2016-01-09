574a7f3a30b71d6ec16c962227bd9da5462771e3
emplumb <emplumb@gmail.com>
2016-01-09 17:03:10 -0600

    removed create and update actions

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,7 +1,4 @@
 class TermsController < ApplicationController
-  def index
-    @terms = Term.all
-  end
 
   def show
 
@@ -10,79 +7,12 @@ class TermsController < ApplicationController
   end
 
   def directory
-    @terms = Term.all
-    # @term = Term.find_by_name(params[:name])
-
-    # @term.letter = (params[:letter])
-    # @alphabet = ('A'..'Z').to_a
-
-    # @terms = Term.where("name LIKE ?%", params[:letter])
-
-    # @terms.find_each do |directory|
-    #   if @term.to_s.include? 'z'
-    #     render 'directory/b'
-    #   end
-    # end
-  end
-
-  def new
-    @term = Term.new
-  end
-
-  def create
-    @term = Term.new
-    @term.name = params[:name]
-    @term.gender = params[:gender]
-    @term.part_of_speech = params[:part_of_speech]
-    @term.definition = params[:definition]
-    @term.etymology1 = params[:etymology1]
-    @term.etymology2 = params[:etymology2]
-    @term.uses = params[:uses]
-    @term.romance_cognates = params[:romance_cognates]
-    @term.notes1 = params[:notes1]
-    @term.notes2 = params[:notes2]
-    @term.quote1 = params[:quote1]
-    @term.quote2 = params[:quote2]
-
-    if @term.save
-      redirect_to "/terms", :notice => "Term created successfully."
-    else
-      render 'new'
+    if params[:letter].present?
+      puts "gggggggggggggggggggggggggggggggggggggggggggggggggggggggg"
     end
+    @terms = Term.all.order(:name)
+
   end
 
-  def edit
-    @term = Term.find(params[:id])
-  end
 
-  def update
-    @term = Term.find(params[:id])
-
-    @term.name = params[:name]
-    @term.gender = params[:gender]
-    @term.part_of_speech = params[:part_of_speech]
-    @term.definition = params[:definition]
-    @term.etymology1 = params[:etymology1]
-    @term.etymology2 = params[:etymology2]
-    @term.uses = params[:uses]
-    @term.romance_cognates = params[:romance_cognates]
-    @term.notes1 = params[:notes1]
-    @term.notes2 = params[:notes2]
-    @term.quote1 = params[:quote1]
-    @term.quote2 = params[:quote2]
-
-    if @term.save
-      redirect_to "/terms", :notice => "Term updated successfully."
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @term = Term.find(params[:id])
-
-    @term.destroy
-
-    redirect_to "/terms", :notice => "Term deleted."
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,76 +2,16 @@ Rails.application.routes.draw do
   # Routes for the Term resource:
   # resources :terms
   # READ
-  root "terms#index"
+  root "terms#home"
 
-  get "/terms", :controller => "terms", :action => "index"
+  # get "/terms", :controller => "terms", :action => "index"
 
   get "/directory", :controller => "terms", :action => "directory"
   get "directory/term/:name", :controller => "terms", :action => "show"
   # get "/term/:id", :controller => "terms", :action => "show"
 
+  get "/directory/:letter", :controller => "terms", :action => "directory"
 
-  # CREATE
-  get "/terms/new", :controller => "terms", :action => "new"
-  post "/create_term", :controller => "terms", :action => "create"
 
-  # UPDATE
-  get "/terms/:id/edit", :controller => "terms", :action => "edit"
-  post "/update_term/:id", :controller => "terms", :action => "update"
 
-  # DELETE
-  get "/delete_term/:id", :controller => "terms", :action => "destroy"
-  #------------------------------
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
