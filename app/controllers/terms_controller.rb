e278568a50b6912a1982980bd87d0e3a742053b5
emplumb <emplumb@evans-mbp.attlocal.net>
2016-01-19 23:01:35 -0600

    basic search working

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,18 +1,27 @@
 class TermsController < ApplicationController
 
   def show
-
     # @term = Term.find(params[:id])
     @term = Term.find_by_name(params[:name])
   end
 
   def directory
+
+#second if/then statement negates first one
     if params[:letter].present?
-      #Is .order(:name) necessary?
       @terms = Term.where(first: params[:letter]).order(:name)
     else
       @terms = Term.all.order(:name)
     end
+#is nil necessary?
+    if params[:q].nil? || params[:q].blank?
+      @terms = Term.all.order(:name)
+    else
+      @terms = Term.search params[:q]
+    end
+
+
+
   end
 
 
