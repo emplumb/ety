d8d7733f6dcd9db22ac460bbf2cea561833137ff
emplumb <emplumb@gmail.com>
2016-04-25 20:01:04 -0500

    Updates home page layout
    Changes homepage to have Word of Day

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,9 +1,24 @@
 class TermsController < ApplicationController
 
+  def daystamp
+    Time.now.strftime("%y%m%d").to_i
+  end
+
   def home
+    number_generator = Random.new(daystamp)
+    min_term_id = Term.minimum(:id)
+    max_term_id = Term.maximum(:id)
+    random_term_id = number_generator.rand(min_term_id..max_term_id)
+    # this random_term_id will always be the same until daystamp changes
+    # or a new Term is added or removed from the database
+
+    @term = Term.find(random_term_id)
   end
 
   def about
+  end
+
+  def faq
   end
 
   def contact
@@ -22,7 +37,7 @@ class TermsController < ApplicationController
   end
 
   def search
-      @terms = Term.search(params[:query]).page(params[:page])
+    @terms = Term.search(params[:query]).page(params[:page])
   end
 
 
