a5674bd1c18fa020093720841b4af1db51db62a5
emplumb <emplumb@gmail.com>
2016-04-25 19:59:36 -0500

    Adds sent email notice to contact form

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,7 +2,8 @@ class ContactController < ApplicationController
 
   def submit_message
     ContactMailer.submit(params[:firstname] + " " + params[:lastname], params[:email], params[:subject], params[:body]).deliver_now
-    redirect_to("/email_confirmation")
+    redirect_to root_path, :notice => "Thanks for your email! Your message has been received."
+
   end
 
 
