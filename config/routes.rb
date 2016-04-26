d9d6aabd1f501625f91add891bd07e3bfb64186a
emplumb <emplumb@gmail.com>
2016-04-25 20:53:12 -0500

    Cleans up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # Routes for the Term resource
-  # resources :terms
 
-  # READ
   root "terms#home"
 
   get "/about", :controller => "terms", :action => "about"
@@ -17,18 +14,10 @@ Rails.application.routes.draw do
 
   get "/term/:name", :controller => "terms", :action => "show"
 
-  # get "/terms/search", :controller => "terms", :action => "search"
-  # turns into:
   get "search" => "terms#search"
 
   post "/submit_message", :controller => "contact", :action => "submit_message"
 
   get "/email_confirmation", :controller => "contact", :action => "email_confirmation"
-
-
-  #revisit below
-  # get "/directory/search", :controller => "terms", :action => "search"
-
-  # get "/directory/search", to: "directory#search", as: "search"
 
 end
