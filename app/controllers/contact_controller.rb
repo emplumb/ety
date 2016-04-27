a477b996f0db712e0890f956cc12eba16eaabade
emplumb <emplumb@gmail.com>
2016-04-26 21:21:33 -0500

    Moves email notice from Application layout to Contact form

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,11 +2,6 @@ class ContactController < ApplicationController
 
   def submit_message
     ContactMailer.submit(params[:firstname] + " " + params[:lastname], params[:email], params[:subject], params[:body]).deliver_now
-    redirect_to root_path, :notice => "Thanks for your email! Your message has been received."
-
+    redirect_to contact_path, :notice => "Thanks for your email! Your message has been received."
   end
-
-
-
-
 end
