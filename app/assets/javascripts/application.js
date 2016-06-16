089295bc9fd5c45bfd0b117db7b4802a2e3fd61e
emplumb <emplumb@gmail.com>
2016-06-15 23:46:18 -0500

    Fixes Read More/Less toggle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,14 +15,23 @@
 
 $(document).ready(function() {
 
+  // Hide the extra content initially
+  $('.read-more-content').addClass('hide')
+  $('.read-more-show, .read-more-hide').removeClass('hide')
 
-  $(".expand").click(function() {
-    event.preventDefault();
-    $(".background").toggleClass("background-truncate");
+  // Set up the toggle effect:
+  $('.read-more-show').on('click', function(e) {
+    $(this).next('.read-more-content').removeClass('hide');
+    $(this).addClass('hide');
+    e.preventDefault();
+  });
 
-     if ($(this).text() == "Read More")
-       $(this).text("Collapse")
-    else
-       $(this).text("Read More");
+  // Changes contributed by @diego-rzg
+  $('.read-more-hide').on('click', function(e) {
+    var p = $(this).parent('.read-more-content');
+    p.addClass('hide');
+    p.prev('.read-more-show').removeClass('hide');
+    // Hide only the preceding "Read More"
+    e.preventDefault();
   });
 });
