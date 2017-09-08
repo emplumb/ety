2526bfa04a68114e2f3ca120f70f04d48aeb8c11
emplumb <emplumb@gmail.com>
2017-09-08 00:21:50 -0500

    Created Sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,13 @@ Rails.application.routes.draw do
   root "terms#home"
 
   get "/about", to: "terms#about"
-
   get "/appendix", to: "terms#appendix"
-
   get "/phonology", to: "terms#phonology"
-
   get "/sources", to: "terms#sources"
-
   get "/contact", to: "terms#contact"
-
   get "/directory", to: "terms#directory"
-
   get "/directory/:letter", to: "terms#directory"
-
   get "/term/:id", to: "terms#show"
-
   get "/search", to: "terms#search"
 
   #Contact route
@@ -26,9 +18,12 @@ Rails.application.routes.draw do
 
   #Errors routes
   match "/404", to: "errors#not_found", :via => :all
-
   match "/422", to: "errors#unacceptable", :via => :all
-
   match "/500", to: "errors#internal_server_error", :via => :all
+
+  #Sessions routes
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
 
 end
