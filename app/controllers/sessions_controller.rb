9dafd64d0f32cf3f201d245d0e2f9b0b8a693caa
emplumb <emplumb@gmail.com>
2017-09-09 02:01:29 -0500

    Added indication of admin login in navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,20 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = 'Successfully logged in!'
       redirect_to '/'
     else
-      flash[:warning] = 'Invalid email or password!'
+      flash[:warning] = 'Invalid username or password'
       redirect_to '/login'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:warning] = 'Successfully logged out!'
+    flash[:success] = 'Successfully logged out'
     redirect_to '/login'
   end
 end
