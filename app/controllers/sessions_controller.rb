b9f3284b4bcfd4b1c409cc4be256a5386e4ea584
emplumb <emplumb@gmail.com>
2017-09-26 22:04:44 -0500

    Prettied up login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:success] = 'Successfully logged in!'
+      flash[:success] = 'Successfully logged in'
       redirect_to '/'
     else
       flash[:warning] = 'Invalid username or password'
