383718631bec39f12c37e55156c677e86f0a36ca
emplumb <emplumb@gmail.com>
2017-10-24 23:25:30 -0500

    Revert "Moved some Terms controller actions to HomeUpdates"
    This reverts commit 6424eb864e3d43eba90043a6c1332355e6b5d646.

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,6 +1,24 @@
 class TermsController < ApplicationController
   before_action :authenticate_admin!, only: [:edit, :update]
 
+  def daystamp
+    Time.now.strftime("%y%m%d").to_i
+  end
+
+  def home
+    number_generator = Random.new(daystamp)
+    min_term_id = Term.minimum(:id)
+    max_term_id = Term.maximum(:id)
+
+    #Will always be the same until daystamp changes or if a new Term is added or removed from the database
+    random_term_id = number_generator.rand(min_term_id..max_term_id)
+
+    @term = Term.find(random_term_id)
+  end
+
+  def about
+  end
+
   def contact
   end
 
@@ -8,6 +26,9 @@ class TermsController < ApplicationController
   end
 
   def appendix
+  end
+
+  def phonology
   end
 
   def show
