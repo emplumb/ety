bfd8c62809914600dbde4474cbb350adca9a167b
emplumb <emplumb@gmail.com>
2017-10-24 23:24:43 -0500

    Revert "Fix for updating terms only"
    This reverts commit ac5ec31d02d207624ea2b230098191d213078458.

--- app/models/term.rb
+++ app/models/term.rb
@@ -2,17 +2,13 @@ require 'elasticsearch/model'
 
 class Term < ActiveRecord::Base
   validates :name, presence: true
-  belongs_to :user
-  before_update :update_slug, unless: :name_not_changed?
-  before_save :update_prefix
 
-  def name_not_changed?
-    !self.name_changed?
-  end
+  belongs_to :user
+
+  before_save :update_slug, :update_prefix
 
   def update_slug
-    param_name = name.parameterize
-    self.slug = get_valid_slug(param_name, index)
+    self.slug = name.parameterize
   end
 
   def to_param
@@ -23,16 +19,6 @@ class Term < ActiveRecord::Base
   ENYE_PREFIX = ['ñ']
 
   private
-    def get_valid_slug(name, index)
-      index_name = index > 1 ? name + "-" + index.to_s : name
-
-      if Term.exists?(slug: index_name)
-        return get_valid_slug(name, index + 1)
-      end
-
-      return index_name
-    end
-
     def update_prefix
       self.prefix = MULTI_PREFIX.detect do |multi|
         self.slug.start_with?(multi)
