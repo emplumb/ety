381e354c85aa9f01ac841288598a6c2ce3b90ae9
emplumb <emplumb@gmail.com>
2017-11-30 19:47:59 -0600

    Limit destroy access to admin

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,5 +1,5 @@
 class TermsController < ApplicationController
-  before_action :authenticate_admin!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_admin!, only: [:new, :create, :edit, :update, :destroy]
 
   def daystamp
     Time.now.strftime("%y%m%d").to_i
