3001ca31ad5d4bf8525641735cd4997b8cf2695f
emplumb <emplumb@gmail.com>
2017-12-06 20:22:41 -0600

    Updated received email formatting and contact form formatting

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,12 +6,16 @@ class ContactMailer < ApplicationMailer
   def submit(fullname, email, subject, body)
     mg_client = Mailgun::Client.new 'key-9daa213cdd6cec1de0e4a8e279e17696'
     message_params = {
-      :from => 'Mailgun Sandbox <postmaster@sandbox0a89f84c7faf43aead8c7df561124480.mailgun.org>',
+      :from => "#{fullname} <postmaster@sandbox0a89f84c7faf43aead8c7df561124480.mailgun.org>",
       :to => "oedosproject@gmail.com",
       :subject => "#{subject}",
-      :html => "<h4>Name:</h4><p>#{fullname}</p><h4>Email:</h4><p>#{email}</p><h4>Subject:</h4><p>#{subject}</p><h4>Body:</h4><p>#{body}</p>"
+      :html => 
+      "<p><strong>Name:</strong> #{fullname}</p>
+      <p><strong>Email:</strong> #{email}</p>
+      <p><strong>Subject:</strong> #{subject}</p>
+      <p><strong>Body:</strong> <p>#{body}</p>"
     }
-    mg_client.send_message 'sandbox0a89f84c7faf43aead8c7df561124480.mailgun.org', message_params
+    mg_client.send_message 'www.spanishetym.com', message_params
 
   end
 
