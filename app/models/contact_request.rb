8e31c525df7ab0fb593da2c8ce7f96d9fe918038
emplumb <emplumb@gmail.com>
2017-12-06 20:21:43 -0600

    Updated logic in contact_requests controller and model

--- app/models/contact_request.rb
+++ app/models/contact_request.rb
@@ -1,2 +1,6 @@
 class ContactRequest < ApplicationRecord
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :first_name, :last_name, :subject, :body, presence: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }
 end
