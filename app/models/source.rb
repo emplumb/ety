8d0d0a99141f3374217acbf2d84d9b8cb55fc25a
emplumb <emplumb@gmail.com>
2017-12-17 14:39:59 -0600

    Added validation for at least one present source attribute

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,4 +1,11 @@
 class Source < ApplicationRecord
+	validate :any_field_present?, on: :create
+
+	def any_field_present?
+	  if %w(author article other book journal printing year website).all? { |attr| self[attr].blank? }
+	    errors.add :base, "At least one field must be present"
+	  end
+	end
 
 	private
 		def self.sort_all
