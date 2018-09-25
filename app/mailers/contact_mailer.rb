9f83172ab9e9ecd74c42fe9b2d98f0a086453fd0
emplumb <emplumb@gmail.com>
2018-09-24 23:10:09 -0500

    Updated ContactMailer formatting
    simple_format to replace \n with p tags

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,22 +4,17 @@ class ContactMailer < ApplicationMailer
   default from: 'oedosproject@gmail.com'
 
   def submit(fullname, email, subject, body)
-    mg_client = Mailgun::Client.new 'key-9daa213cdd6cec1de0e4a8e279e17696'
-    message_params = {
-      :from => "#{fullname} <postmaster@sandbox0a89f84c7faf43aead8c7df561124480.mailgun.org>",
+    mg_client = Mailgun::Client.new("key-9daa213cdd6cec1de0e4a8e279e17696")
+    formatted_body = ActionController::Base.helpers.simple_format(body)
+
+    message_params = 
+    {
+      :from => "#{fullname} <#{email}>",
       :to => "oedosproject@gmail.com",
       :subject => "#{subject}",
-      :html => 
-      "<p><strong>Name:</strong> #{fullname}</p>
-      <p><strong>Email:</strong> #{email}</p>
-      <p><strong>Subject:</strong> #{subject}</p>
-      <p><strong>Body:</strong> <p>#{body}</p>"
+      :html => "#{formatted_body}"
     }
+
     mg_client.send_message 'www.spanishetym.com', message_params
-
   end
-
-
-
-
 end
