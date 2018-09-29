bfca25f517274b663453d18f78098af880d1b43a
emplumb <emplumb@gmail.com>
2018-09-29 11:44:25 -0500

    Update AboutPage model spec

--- spec/models/about_page_spec.rb
+++ spec/models/about_page_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe AboutPage, type: :model do
-	it "is not valid without a body" do
-		page = AboutPage.create
-	  expect(page).to_not be_valid
+	describe 'Validations' do
+		it "is not valid without a body" do
+			page = AboutPage.create
+		  expect(page).to_not be_valid
+		end
 	end
 end
 
