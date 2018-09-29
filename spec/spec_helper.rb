c084a71761c7ba8df20a2bf2a27e56dc6f807f5d
emplumb <emplumb@gmail.com>
2018-09-29 12:52:15 -0500

    HomeUpdates validation tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,9 @@ RSpec.configure do |config|
   # assertions if you prefer.
   config.formatter = :documentation
 
+  # This will display full backtrace
+  # config.full_backtrace = true
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
