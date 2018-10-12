91d5f482f55b26516ca02ebbe6d3507f172f7509
emplumb <emplumb@gmail.com>
2018-10-11 19:36:44 -0500

    inserts Balto-Slavic Citations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181011232508) do
+ActiveRecord::Schema.define(version: 20181012001739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
