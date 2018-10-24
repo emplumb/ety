9c200228fdc44bbb97c2e3f47ffb344e25a1124c
emplumb <emplumb@gmail.com>
2018-10-23 19:14:32 -0500

    Moved presence checks to helper and modified ES

--- app/helpers/terms_helper.rb
+++ app/helpers/terms_helper.rb
@@ -17,4 +17,36 @@ module TermsHelper
 		response ? response.first : result['_source'][attribute]		
 	end
 
+	def etymology?(term_entry)
+		term_entry.etymology1.present? || term_entry.etymology2.present?
+	end
+
+	def ie_family?(term_entry)
+    term_entry.indo_european_cognates.present? || term_entry.romance_cognates.present? ||
+    term_entry.italic_cognates.present? || term_entry.celtic_cognates.present? ||
+    term_entry.germanic_cognates.present? || term_entry.albanian_cognates.present? ||
+    term_entry.balto_slavic_cognates.present? || term_entry.hellenic_cognates.present? ||
+    term_entry.thracian_cognates.present? || term_entry.phrygian_cognates.present? ||
+    term_entry.messapian_cognates.present? || term_entry.armenian_cognates.present? ||
+    term_entry.indo_iranian_cognates.present? || term_entry.tocharian_cognates.present? ||
+    term_entry.anatolian_cognates.present? 
+  end
+
+	def uralic_family?(term_entry)
+		term_entry.uralic_cognates.present? || term_entry.sami_cognates.present? ||
+    term_entry.finnic_cognates.present? || term_entry.mordvinic_cognates.present? ||
+    term_entry.mari_cognates.present? || term_entry.mansi_cognates.present? ||
+    term_entry.khanty_cognates.present?
+	end
+
+	def ne_caucasian_family?(term_entry)
+    term_entry.northeast_caucasian_cognates.present? || term_entry.nakh_cognates.present? ||
+    term_entry.lezgic_cognates.present? || term_entry.dargwa_cognates.present? ||
+    term_entry.lak_cognates.present? || term_entry.lezghian_cognates.present?
+  end
+
+  def afro_asiatic_family?(term_entry)
+    term_entry.afro_asiatic_cognates.present? || term_entry.egyptian_cognates.present? || term_entry.semitic_cognates.present?
+  end
+
 end
--- app/models/term.rb
+++ app/models/term.rb
@@ -28,38 +28,6 @@ class Term < ActiveRecord::Base
     slug
   end
 
-  def etym?
-    etymology1.present? || etymology2.present?
-  end
-
-  def ie_cognates?
-    indo_european_cognates.present? || romance_cognates.present? ||
-    italic_cognates.present? || celtic_cognates.present? ||
-    germanic_cognates.present? || albanian_cognates.present? ||
-    balto_slavic_cognates.present? || hellenic_cognates.present? ||
-    thracian_cognates.present? || phrygian_cognates.present? ||
-    messapian_cognates.present? || armenian_cognates.present? ||
-    indo_iranian_cognates.present? || tocharian_cognates.present? ||
-    anatolian_cognates.present? 
-  end
-
-  def uralic_cognates?
-    uralic_cognates.present? || sami_cognates.present? ||
-    finnic_cognates.present? || mordvinic_cognates.present? ||
-    mari_cognates.present? || mansi_cognates.present? ||
-    khanty_cognates.present?
-  end
-
-  def ne_cognates?
-    northeast_caucasian_cognates.present? || nakh_cognates.present? ||
-    lezgic_cognates.present? || dargwa_cognates.present? ||
-    lak_cognates.present? || lezghian_cognates.present?
-  end
-
-  def aa_cognates?
-    afro_asiatic_cognates.present? || egyptian_cognates.present? || semitic_cognates.present?
-  end
-
   def ie_c
     indo_european_cognates
   end
@@ -124,6 +92,10 @@ class Term < ActiveRecord::Base
           type: 'stemmer',
           name: 'spanish'
         },
+        light_spanish_stemmer: {
+          type: 'stemmer',
+          name: 'light_spanish'
+        },
         definition_stop: {
           type: 'stop',
           stopwords: ['and', 'or', 'a', 'to', 'an']
@@ -154,6 +126,11 @@ class Term < ActiveRecord::Base
           filter: ['lowercase', 'spanish_stemmer'],
           char_filter: ['html_strip', 'accents_char_filter']
         },
+        name_analyzer: {
+          tokenizer: 'classic',
+          filter: ['lowercase', 'light_spanish_stemmer'],
+          char_filter: ['html_strip', 'accents_char_filter']
+        },
         definition_analyzer: {
           tokenizer: 'standard',
           filter: ['lowercase', 'english_stemmer', 'spanish_stemmer', 'definition_stop'],
@@ -163,30 +140,45 @@ class Term < ActiveRecord::Base
     } do
 
     mappings dynamic: 'false' do
-      indexes :id, index: :no
       indexes :slug, analyzer: :spanish_analyzer, index_options: :offsets, store: true
-      indexes :name, analyzer: :spanish_analyzer, index_options: :offsets, store: true
-      indexes :prefix_suffix, index: :no
-      indexes :part_of_speech, index: :no
+      indexes :name, analyzer: :name_analyzer, index_options: :offsets, store: true
       indexes :definition, analyzer: :definition_analyzer, index_options: :offsets, store: true
       indexes :etymology1, analyzer: :combined_analyzer, index_options: :offsets, store: true
       indexes :etymology2, analyzer: :combined_analyzer, index_options: :offsets, store: true
       indexes :variants, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :indo_european_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
       indexes :romance_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
       indexes :italic_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
-      indexes :tyrsenian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
       indexes :celtic_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
       indexes :germanic_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
-      indexes :baltoslavic_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
       indexes :albanian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :balto_slavic_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
       indexes :hellenic_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :thracian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :phrygian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :messapian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
       indexes :armenian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
-      indexes :indoiranian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
-      indexes :semitic, analyzer: :combined_analyzer, index_options: :offsets, store: true
-      indexes :uralic, analyzer: :combined_analyzer, index_options: :offsets, store: true
-      indexes :ne_caucasian, analyzer: :combined_analyzer, index_options: :offsets, store: true
-      indexes :ie_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
-      indexes :uses, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :indo_iranian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :tocharian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :anatolian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :basque_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :tyrsenian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :uralic_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :sami_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :finnic_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :mordvinic_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :mari_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :mansi_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :khanty_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :northeast_caucasian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :nakh_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :lezgic_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :dargwa_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :lak_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :lezghian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :afro_asiatic_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :egyptian_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
+      indexes :semitic_cognates, analyzer: :combined_analyzer, index_options: :offsets, store: true
       indexes :notes1, analyzer: :combined_analyzer, index_options: :offsets, store: true
       indexes :notes2, analyzer: :combined_analyzer, index_options: :offsets, store: true
     end
@@ -197,10 +189,14 @@ class Term < ActiveRecord::Base
       query: {
         multi_match: {
           query: query,
-          fields: ['slug^10','name^20', 'definition^8', 'etymology1^7', 'etymology2^6', 'uses^4', 'variants^3', 'romance_cognates^3', 'italic_cognates^3', 'celtic_cognates^3', 'germanic_cognates^3', 'albanian_cognates^3', 'balto_slavic_cognates^3', 'hellenic_cognates^3', 'thracian_cognates^3', 'phrygian_cognates^3', 'messapian_cognates^3', 'armenian_cognates^3', 'indo_iranian_cognates', 'tocharian_cognates', 'anatolian_cognates', 'semitic^3', 'uralic^3', 'ne_caucasian^3', 'ie_cognates^3', 'tyrsenian_cognates^3', 'notes1^2', 'notes2^1'],
+          fields: ['slug^10','name^50', 'definition^15', 'etymology1^7', 'etymology2^6', 'uses^4', 'variants^3', 'indo_european_cognates^3', 'romance_cognates^3', 'italic_cognates^3', 'celtic_cognates^3', 'germanic_cognates^3', 'albanian_cognates^3', 'balto_slavic_cognates^3', 'hellenic_cognates^3', 'thracian_cognates^3', 'phrygian_cognates^3', 'messapian_cognates^3', 'armenian_cognates^3', 'indo_iranian_cognates^3', 'tocharian_cognates^3', 'anatolian_cognates^3', 'basque_cognates^3', 'tyrsenian_cognates^3', 'uralic_cognates^3', 'sami_cognates^3', 'finnic_cognates^3', 'mordvinic_cognates^3', 'mari_cognates^3', 'mansi_cognates^3', 'khanty_cognates^3', 'northeast_caucasian_cognates^3', 'nakh_cognates^3', 'lezgic_cognates^3', 'dargwa_cognates^3', 'lak_cognates^3', 'lezghian_cognates^3', 'afro_asiatic_cognates^3', 'egyptian_cognates^3', 'semitic_cognates^3', 'notes1^2', 'notes2^1'],
           operator: 'AND'
         }
       },
+      sort: [
+        { _score: {order: 'desc'} },
+        { name: {order: 'asc'} }
+      ],
       highlight: {
         pre_tags: ['<em>'],
         post_tags: ['</em>'],
@@ -210,22 +206,41 @@ class Term < ActiveRecord::Base
           definition: {},
           etymology1: {},
           etymology2: {},
-          variants: {},
           uses: {},
+          variants: {},
+          indo_european_cognates: {},
           romance_cognates: {},
           italic_cognates: {},
-          tyrsenian_cognates: {},
           celtic_cognates: {},
           germanic_cognates: {},
-          baltoslavic_cognates: {},
           albanian_cognates: {},
+          balto_slavic_cognates: {},
           hellenic_cognates: {},
+          thracian_cognates: {},
+          phrygian_cognates: {},
+          messapian_cognates: {},
           armenian_cognates: {},
-          indoiranian_cognates: {},
-          semitic: {},
-          uralic: {},
-          ne_caucasian: {},
-          ie_cognates: {},
+          indo_iranian_cognates: {},
+          tocharian_cognates: {},
+          anatolian_cognates: {},
+          basque_cognates: {},
+          tyrsenian_cognates: {},
+          uralic_cognates: {},
+          sami_cognates: {},
+          finnic_cognates: {},
+          mordvinic_cognates: {},
+          mari_cognates: {},
+          mansi_cognates: {},
+          khanty_cognates: {},
+          northeast_caucasian_cognates: {},
+          nakh_cognates: {},
+          lezgic_cognates: {},
+          dargwa_cognates: {},
+          lak_cognates: {},
+          lezghian_cognates: {},
+          afro_asiatic_cognates: {},
+          egyptian_cognates: {},
+          semitic_cognates: {},
           notes1: {},
           notes2: {}
         }
