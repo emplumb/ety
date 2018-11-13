54aa87ca98e4bc42db9a4d61b5905c7f8ff7d376
emplumb <emplumb@gmail.com>
2018-11-13 13:10:00 -0600

    Add relevant page titles to each view

---
+++ app/helpers/directory_helper.rb
@@ -0,0 +1,15 @@
+module DirectoryHelper
+
+	def page_title(letter_param)
+		if !letter_param.present?
+			return
+		end
+
+		if letter_param == 'ñ'
+			return 'Ñ'
+		else
+			return letter_param.upcase
+		end
+	end
+
+end
