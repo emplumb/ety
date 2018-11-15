e509074f31fd1e169af0cf5385ec0baa0c065c5a
emplumb <emplumb@gmail.com>
2018-11-14 22:06:24 -0600

    Removed required term presence from citation for newly created terms

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -2,7 +2,7 @@ class Citation < ApplicationRecord
   belongs_to :term
   belongs_to :source
 
-  validates_presence_of :term, :source
+  validates_presence_of :source
   validates :term_id, uniqueness: { scope: :source_id, message: "This source has already been associated to this entry" }
 
 end
